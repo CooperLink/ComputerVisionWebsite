2811f057b6f9379a8405549e0598aa637fd8189c
Matt Carroll <mwcarroll12@gmail.com>
2020-11-19 12:11:45 -0500

    added feature detection

---
+++ internals/face_features.py
@@ -0,0 +1,25 @@
+import dlib
+import numpy as np
+import imageio
+import matplotlib.pyplot as plt
+
+predictor_path = 'shape_predictor_81_face_landmarks.dat'
+
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor(predictor_path)
+
+frame = imageio.imread("./zvi.jpg")
+dets = detector(frame, 1)
+faces = []
+for d in dets:
+    shape = predictor(frame, d)
+    landmarks = np.array([[p.x, p.y] for p in shape.parts()])
+    faces.append(landmarks)
+
+
+implot = plt.imshow(frame)
+
+for face in faces:
+    plt.scatter(x=face[:,0], y=face[:,1], s=5)
+
+plt.show()
